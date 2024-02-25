d15ccb2a49eae2a6e32683cff59cf645edfcdd08
faviio lopez <fa.lopezz@uea.edu.ec>
2024-02-24 19:53:27 -0500

    deber semana 11

---
+++ semana 11.py
@@ -0,0 +1,9 @@
+mi_lista = [ 7, 9 , 19, 67 ]
+
+elemento_a_buscar= 19
+if elemento_a_buscar in mi lista:
+    print(f"{elemento_a_buscar} se encuentra en mi_lista.")
+
+else:
+
+    print(f"elemento_a_buscar} no esta en mi_lista.")
