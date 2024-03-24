e131f733dbeb1af5d8f9b15fdce9d19266f590df
faviio lopez <fa.lopezz@uea.edu.ec>
2024-03-24 13:23:36 -0500

    deber semana 15

---
+++ deber semana 15.py
@@ -0,0 +1,12 @@
+#informacion personal
+informacion_personal = [
+    {"nombre": "graciela", "apellido", "piguave", "ciudad", "playas de villamil", "edad", "24", "profesion", "licenciada"},
+    {"nombre": "andy", "apellido", "mero", "ciudad", "la union", "edad", "18", "profesion", "estudiante"},
+    {"nombre": "favio", "apellido", "lopez", "ciudad", "playas villamil", "edad", "28", "profesion", "chofer"},
+    {"nombre": "geampool", "apellido", "garcia", "ciudad", "playas villamil", "edad", "25", "profesion", "chofer"}
+]
+print(informacion_personal [0] ["nombre"])
+telephone= "telefono"
+if telephone not in informacion_personal:
+    print("el telefono no existe")
+else: print ("el telefono")
